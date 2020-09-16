b7c429443f2dde5c3b7106da62b37302ea923c7a
Niko o <kolyabokov41@gmail.com>
2020-09-16 17:54:21 +0300

    connect to mongo db, api/users

---
+++ server/lib/api-helpers.js
@@ -0,0 +1,19 @@
+// take only needed user fields to avoid sensitive ones (such as password)
+export function extractUser(req) {
+  if (!req.user) return null;
+  const {
+    _id, name, email, bio, profilePicture, emailVerified,
+  } = req.user;
+  return {
+    _id, name, email, bio, profilePicture, emailVerified,
+  };
+}
+
+export const apiHandler = (res, method, handlers) => {
+  if (!Object.keys(handlers).includes(method)) {
+    res.setHeader('Allow', Object.keys(handlers))
+    res.status(405).end(`Method ${method} Not Allowed`)
+  } else {
+    handlers[method](res)
+  }
+}
---
+++ server/middlewares/connect-to-mongo.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+// we'll import all the schemas here and return them
+// on the mongo connection object
+// for use in the handlers
+// import UserSchema from '../../data/models/User';
+import UserSchema from '../models/Users';
+
+const connectToMongo = async () => {
+  const connection = await mongoose.createConnection(
+    process.env.MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useUnifiedTopology: true
+    }
+  );
+  const User = connection.model("User", UserSchema);
+  return {
+    connection,
+    models: {
+      User
+    }
+  };
+}
+
+export default connectToMongo;
